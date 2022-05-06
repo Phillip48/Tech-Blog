b9c57ac23d7024656daa1e36739a48c5436bd507
Phillip48 <phillippereira02@gmail.com>
2022-05-06 19:20:48 -0400

    Added helpers

--- utils/auth.js
+++ utils/auth.js
@@ -4,6 +4,12 @@ const withAuth = (req,res,next) => {
     //if (con) - if youre not logged in
     // then redirect to login page
     // else move on to your dashboard
+    if (!req.session.loggedIn) {
+        res.redirect('/login');
+      } else {
+        // We call next() if the user is authenticated
+        next();
+      }
 };
 
 module.exports = withAuth;
