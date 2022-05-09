2c8b5636f0b622ed8d0ef2768a11ba8767a887f9
Phillip48 <phillippereira02@gmail.com>
2022-05-08 23:08:41 -0400

    Worked on handlebars

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,21 +1,27 @@
-const signupFormHandler = async function(event) {
-    event.preventDefault();
-  
-    const usernameEl = document.querySelector('#username-input-signup');
-    const passwordEl = document.querySelector('#password-input-signup');
-  
-    const response = await fetch('/api/user', {
-         // Create the functionality to help create the buttons for your website.
+const signupFormHandler = async function (event) {
+  event.preventDefault();
 
+  const usernameEl = document.querySelector('#username-input-signup');
+  const passwordEl = document.querySelector('#password-input-signup');
+
+
+
+  if (usernameEl && passwordEl) {
+    const response = await fetch('/api/user', {
+      method: 'POST',
+      body: JSON.stringify({ usernameEl, passwordEl }),
+      headers: { 'Content-Type': 'application/json' },
     });
-  
+
+
     if (response.ok) {
       document.location.replace('/dashboard');
     } else {
       alert('Failed to sign up');
     }
-  };
-  
-  document
-    .querySelector('#signup-form')
-    .addEventListener('submit', signupFormHandler);
+  }
+};
+
+document
+  .querySelector('#signup-form')
+  .addEventListener('submit', signupFormHandler);
